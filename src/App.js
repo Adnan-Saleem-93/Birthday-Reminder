ad9a206fc120a375a1b7eacfbc0835e0d577b6fa
Adnan Saleem <adnan13893@gmail.com>
2021-05-06 11:41:37 +0500

    UI changes & extra code remove, added netlify link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import BirthdayItem from './birthdayItem';
 
 function App() {
   const [birthdayList, setBirthdayList] = useState(birthdays); // array destructuring
-  const [count, setCount] = useState(birthdayList.length);
 
   // this function returns the HTML returned from the .map function
   // for each birthday item
@@ -18,14 +17,13 @@ function App() {
 
   const onClearAll = () => {
     setBirthdayList([]);
-    setCount(0);
   };
 
   return (
     <div className="App">
-      <h3 className="text-secondary">{count} Birthday(s) today</h3>
+      <h3 className="text-secondary">{birthdayList.length} Birthday(s) today</h3>
       {renderBirthdayList()}
-      <Button variant="primary" block onClick={onClearAll}>
+      <Button variant="secondary" block onClick={onClearAll}>
         Clear All Birthdays
       </Button>
     </div>
